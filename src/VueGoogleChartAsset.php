a89bc6731eb774cceb7a956d3e3e2f930461a589
Jomon Johnson <jomon.entero@gmail.com>
2019-08-08 17:33:05 +0530

    init

--- src/VueGoogleChartAsset.php
+++ src/VueGoogleChartAsset.php
@@ -4,14 +4,18 @@ namespace codexten\yii\web\assets\VueGoogleChart;
 
 use codexten\yii\web\assets\vue\VueAsset;
 use yii\web\AssetBundle;
+use yii\web\View;
 
 class VueGoogleChartAsset extends AssetBundle
 {
     public $sourcePath = '@npm/vue-google-charts/dist';
-    public $js = [
-        'vue-google-charts.browser.js',
-    ];
+//    public $js = [
+//        'vue-google-charts.browser.js',
+//    ];
     public $depends = [
         VueAsset::class,
+    ];
+    public $jsOptions = [
+        'position' => View::POS_HEAD,
     ];
 }
